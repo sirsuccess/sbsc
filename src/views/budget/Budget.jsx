1ffaf96c6dee395e1a3f87c6354962f849ec642e
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-03-20 09:43:52 +0100

    created budget component

---
+++ src/views/budget/Budget.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import DasboardShell from "../../components/features/dashboard-shell/DashboardShell"
+
+export default function Budget(params) {
+    return (
+        <DasboardShell>
+            budget
+        </DasboardShell>
+    
+    )
+}
