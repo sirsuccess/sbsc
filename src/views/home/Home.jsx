e29dcb3214084685f177e0a2c15a33b63924a97e
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-03-20 17:43:51 +0100

    modify Home

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+
 import DasboardShell from "../../components/features/dashboard-shell/DashboardShell"
+import HomeFirstSec from "../../components/features/homeFirstSec/homeFirstSec"
+import DashboardDate from "../../components/commons/DashboardDate/DashboardDate"
+
+
 
 export default function Home(params) {
     return (
         <DasboardShell>
-            Home
+            <DashboardDate/>
+            <HomeFirstSec/>
         </DasboardShell>
     
     )
