e51b0f90aa88049a4f267c29b22a8346817bf4b4
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-03-20 17:42:09 +0100

    created Reassignment

---
+++ src/Data/ReassignmentData.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        img: "./assets/icons/1.svg",
+        title: "Pending Budget Reassignment Status",
+        value: 200
+    },
+    {
+        img: "./assets/icons/2.svg",
+        title: "Approved Budget Reassignment Status",
+        value: 373
+    },
+    {
+        img: "./assets/icons/3.svg",
+        title: "Declined Budget Reassignment Status",
+        value: 372
+    },
+]
