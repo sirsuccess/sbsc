6c4cf3095f041e2e1eb696a7c5542ff159fb223b
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-03-20 09:44:22 +0100

    created approval component

---
+++ src/views/approval/Approval.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import DasboardShell from "../../components/features/dashboard-shell/DashboardShell"
+
+export default function Approval(params) {
+    return (
+        <DasboardShell>
+            Approval
+        </DasboardShell>
+    
+    )
+}
