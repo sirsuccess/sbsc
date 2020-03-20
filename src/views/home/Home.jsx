7f0a4e098311e4b6a07e7245810543eb10bacda0
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-03-20 09:43:19 +0100

    home route created

---
+++ src/views/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import DasboardShell from "../../components/features/dashboard-shell/DashboardShell"
+
+export default function Home(params) {
+    return (
+        <DasboardShell>
+            Home
+        </DasboardShell>
+    
+    )
+}
