a2416b424f261df214099bb9c10fdfc2000ad5b7
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-03-21 19:57:58 +0100

    add total

---
+++ src/components/commons/cardTotal/CardTotal.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./CardTotal.css";
+
+export default function CardTotal({ transaction }) {
+  const { name, spentImg, spentAmount, balImag, balAmount } = transaction;
+  return (
+    <div className="CardTotal">
+      <div className="spentBig">
+        <img src={spentImg} alt="transaction spending" />
+        <div className="innerSpent">
+          <div className="spentTitle">Total <span>{name}</span> Spent</div>
+        <div className="spentVal">N{spentAmount}</div>
+        </div>
+      </div>
+      <div className="spentBig">
+        <img src={balImag} alt="transaction spending" />
+        <div className="innerSpent">
+          <div className="spentTitle">Total <span>{name}</span> Balance</div>
+        <div className="spentVal">N{balAmount}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
