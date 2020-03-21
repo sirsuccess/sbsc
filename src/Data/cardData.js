9dce5519f07d5c7123b09c8c9c9cc3994a748594
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-03-21 15:09:05 +0100

    update card data and chart data

---
+++ src/Data/cardData.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        img: "./assets/icons/1.svg",
+        name: "CAPEX",
+        value: "50,000,000",
+        ValBgColor: rgba(81, 44, 98, 0.2),
+        percentage: 64,
+        circleColor: "#6915cf",
+        spentImg: "./assets/icons/1.svg",
+        spentAmount: "18,000,000",
+        balImag: "./assets/icons/1.svg",
+        balAmount: "32,000,000"
+    },
+    {
+        img: "./assets/icons/1.svg",
+        name: "OPEX",
+        value: "20,000,000",
+        ValBgColor: rgba(31, 171, 137, 0.2),
+        percentage: 50,
+        circleColor: "#fb0091",
+        spentImg: "./assets/icons/1.svg",
+        spentAmount: "10,000,000",
+        balImag: "./assets/icons/1.svg",
+        balAmount: "10,000,000"
+    },
+    
+]
---
+++ src/Data/chartData.js
@@ -0,0 +1,74 @@
+export default [
+    {
+      name: "Jan",
+      Spent: 3500,
+      Balance: 2400,
+      amt: 2400
+    },
+    {
+      name: "Feb",
+      Spent: 4000,
+      Balance: 2398,
+      amt: 2210
+    },
+    {
+      name: "Mar",
+      Spent: 4500,
+      Balance: 3800,
+      amt: 2290
+    },
+    {
+      name: "Apr",
+      Spent: 4780,
+      Balance: 3908,
+      amt: 2000
+    },
+    {
+      name: "May",
+      Spent: 1890,
+      Balance: 4800,
+      amt: 2181
+    },
+    {
+      name: "June",
+      Spent: 2390,
+      Balance: 3800,
+      amt: 2500
+    },
+    {
+      name: "July",
+      Spent: 3490,
+      Balance: 4300,
+      amt: 2100
+    },
+    {
+      name: "Aug",
+      Spent: 3490,
+      Balance: 4300,
+      amt: 2100
+    },
+    {
+      name: "Sept",
+      Spent: 3490,
+      Balance: 4300,
+      amt: 2100
+    },
+    {
+      name: "Oct",
+      Spent: 3490,
+      Balance: 4300,
+      amt: 2100
+    },
+    {
+      name: "Nov",
+      Spent: 3490,
+      Balance: 4300,
+      amt: 2100
+    },
+    {
+      name: "Dec",
+      Spent: 3490,
+      Balance: 4300,
+      amt: 2100
+    }
+  ];
